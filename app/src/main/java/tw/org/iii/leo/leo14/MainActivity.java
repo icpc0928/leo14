c736aac6351a332eb3bbef90ed99943c4c806aee
Leo <icpc0928@gmail.com>
2020-03-21 14:44:48 +0800

    2 json

--- app/src/main/java/tw/org/iii/leo/leo14/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/leo14/MainActivity.java
@@ -1,14 +1,117 @@
 package tw.org.iii.leo.leo14;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private String[] from = {"title","type"};
+    private int[] to = {R.id.item_title,R.id.item_type};
+    private LinkedList<HashMap<String,String>> data = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        initListView();
+        fetchRemoteData();
+
     }
+
+    private void initListView(){
+        adapter = new SimpleAdapter(this,data,R.layout.item,from,to);
+        listView.setAdapter(adapter);
+
+    }
+    //抓遠端資料
+    private void fetchRemoteData(){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https://data.coa.gov.tw/Service/OpenData/RuralTravelData.aspx");
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.connect();
+
+                    BufferedReader reader =
+                            new BufferedReader(
+                                    new InputStreamReader(conn.getInputStream()));
+                    String line; StringBuffer sb = new StringBuffer();
+                    while((line = reader.readLine()) != null){
+                        sb.append(line);
+
+                    }
+                    reader.close();
+                    parseJason(sb.toString());
+
+
+                } catch (Exception e) {
+                    Log.v("leo",e.toString());
+            }
+            }
+        }.start();
+
+    }
+
+    private void parseJason(String json){
+        try {
+            JSONArray root = new JSONArray(json);
+
+            //尋訪“元素”
+            for (int i = 0 ; i<root.length();i++){
+                JSONObject row = root.getJSONObject(i);
+                HashMap<String,String> temp = new HashMap<>();
+                temp.put(from[0],row.getString("Title"));
+                temp.put(from[1],row.getString("TravelType")
+                        .replace('\n',' ')
+                        .replace('\r',' ')
+                        .replace("  ",""));
+                temp.put("pic",row.getString("PhotoUrl"));
+                temp.put("content",row.getString("Contents"));
+                data.add(temp);
+            }
+            //要做到ui 就用Handler
+            uiHandler.sendEmptyMessage(0);
+        } catch (Exception e) {
+            Log.v("leo",e.toString());
+        }
+
+
+    }
+
+
+    private UIHandler uiHandler = new UIHandler();
+    private class UIHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+
 }
